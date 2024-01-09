baf085259976a2369cda8c63a0b08c3a48b0432b
Christoph Rieke <christoph.k.rieke@gmail.com>
2024-01-09 20:14:24 +0100

    move fixture

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,4 +1,8 @@
 import json
+from pathlib import Path
+from typing import List
+
+import pytest
 
 
 def read_geojson(file_path: str, geometries=False):
@@ -7,3 +11,14 @@ def read_geojson(file_path: str, geometries=False):
     if geometries:
         return fc["features"][0]["geometry"]
     return fc
+
+
+@pytest.fixture(scope="module")
+def fixture_geojson_examples_all_normal_files() -> List[Path]:
+    base_path = Path("tests/data")
+    all_normal_files = []
+    for folder in ["valid", "invalid", "problematic"]:
+        paths_in_folder = list(Path(base_path / folder).rglob("*.geojson"))
+        assert paths_in_folder
+        all_normal_files.extend(paths_in_folder)
+    return all_normal_files
